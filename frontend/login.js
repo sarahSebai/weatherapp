a7298bf9bb9335bead914a85f5da17a032e087cb
sebai <sarah.sebai1612@gmail.com>
2025-04-28 14:52:35 +0200

    reglage

--- frontend/login.js
+++ frontend/login.js
@@ -6,7 +6,7 @@ document.querySelector('#register').addEventListener('click', function () {
 		password: document.querySelector('#registerPassword').value,
 	}
 
-	fetch('https://weatherapp-1-iopr.onrender.com/signup', {
+	fetch('http://localhost:3001/users/signup', {
 		method: 'POST',
 		headers: { 'Content-Type': 'application/json' },
 		body: JSON.stringify(user),
@@ -25,7 +25,7 @@ document.querySelector('#connection').addEventListener('click', function () {
 		password: document.querySelector('#connectionPassword').value,
 	}
 
-	fetch('https://weatherapp-1-iopr.onrender.com/signin', {
+	fetch('http://localhost:3001/users/signin', {
 		method: 'POST',
 		headers: { 'Content-Type': 'application/json' },
 		body: JSON.stringify(user),
